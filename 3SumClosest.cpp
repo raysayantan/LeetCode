4ba4cdde4d432d39cd26f36937dabb7a0185c107
Sayantan Ray <ray.sayantan@gmail.com>
2020-03-19 10:32:25 +0530

    Create 3SumClosest.cpp

---
+++ 3SumClosest.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+Example:
+
+Given array nums = [-1, 2, 1, -4], and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int len = nums.size();
+        int currSum = nums[0] + nums[1] + nums[len - 1];
+        sort(nums.begin(), nums.end());
+        int result = currSum;
+        
+        for(int i = 0; i < len; i++){
+            int aPointer = i + 1;
+            int bPointer = len - 1;
+            
+            while(aPointer < bPointer){
+                currSum = nums[aPointer] + nums[bPointer] + nums[i];
+                if(currSum > target){
+                    bPointer--;
+                } else {
+                    aPointer++;
+                }
+                
+                if(abs(currSum - target) < abs(result - target)){
+                    result = currSum;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
