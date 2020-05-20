e82b33dba34279acaf81e75ff87e52856c064f05
Sayantan Ray <ray.sayantan@gmail.com>
2020-05-20 14:47:22 +0530

    Create findMinRotatedSorted.cpp

---
+++ Array/findMinRotatedSorted.cpp
@@ -0,0 +1,44 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+Example 1:
+
+Input: [3,4,5,1,2] 
+Output: 1
+Example 2:
+
+Input: [4,5,6,7,0,1,2]
+Output: 0
+*/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int mid = 0;
+        int start = 0;
+        int l = nums.size();
+        int end = l - 1;
+        
+        if(end == 0) return nums[0];
+        if(nums[end] > nums[0]) return nums[0];
+        
+        while(start <= end){
+            mid = (start + end)/2;
+            if(nums[mid] > nums[mid + 1]) return nums[mid + 1];
+            if(nums[mid] < nums[mid - 1]) return nums[mid];
+            
+            
+            if(nums[mid] > nums[0]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
