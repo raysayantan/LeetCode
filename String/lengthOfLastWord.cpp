7769e318a32516af250ba9871f8fc316803848a5
Sayantan Ray <ray.sayantan@gmail.com>
2020-07-29 03:23:17 +0530

    Create lengthOfLastWord.cpp

---
+++ String/lengthOfLastWord.cpp
@@ -0,0 +1,36 @@
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word 
+(last word means the last appearing word if we loop from left to right) in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+Example:
+
+Input: "Hello World"
+Output: 5
+*/
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        string prev = "";
+        string curr = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == ' '){
+                if(curr != "")
+                    prev = curr;
+                curr = "";
+            } else {
+                curr += s[i];
+            }
+        }
+        
+        if(curr != ""){
+            return curr.length();
+        } else {
+            return prev.length();
+        }
+    }
+};
